53d767fc8067f588278baf30442c9b922b324d3a
Dennis Xiao <dennisboys@gmail.com>
2017-12-21 19:34:01 +1100

    displayed ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+  /* 1. extract the keys from the object and turn them into an array
+     2. represent the amount of each ingredient using the length of an array
+        [Array(1), Array(1), Array(2), Array(2)], each element in the array is 'undefined'
+     3. map each array and convert each 'undefined' into a <BurgerIngredient /> component
+  */
+  const transformedIngredients = Object.keys(props.ingredients).map((ingredientKey) => {
+    return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+      return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   )
