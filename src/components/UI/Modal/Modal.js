e593449e9a44ef86b058fcff1e75b53d1e5aeaf5
Dennis Xiao <dennisboys@gmail.com>
2017-12-23 20:57:56 +1100

    optimize performance for OrderSummary component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,36 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.css';
 
-const modal = (props) => (
-  <Fragment>
-    <Backdrop
-      clicked={props.modalClosed}
-      show={props.show} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Fragment>
-)
+class modal extends Component {
+
+  // use shouldComponentUpdate() to optimize performance
+  // the app should not re-render Modal component when it's hidden
+  // Modal component should be re-rendered only when user clicks 'ORDER NOW' button
+  shouldComponentUpdate(nextProps, nextState) {
+    // Modal component re-render only when the 'show' property changes
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop
+          clicked={this.props.modalClosed}
+          show={this.props.show} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Fragment>
+    )
+  }
+}
 
 export default modal;
