0fa45d1f22cba45623d75cb18373e0ba8adbc21b
Dennis Xiao <dennisboys@gmail.com>
2017-12-25 16:07:28 +1100

    updated navigation

---
+++ src/components/Order/Order.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = (props) => (
+  <div className={classes.Order}>
+    <p>Ingredients: Salad (1)</p>
+    <p>
+      <span>Price:</span>
+      <span>USD 5.45</span>
+    </p>
+  </div>
+);
+
+export default order;
